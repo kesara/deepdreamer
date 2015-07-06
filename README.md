9116676b95e8d7f4853be802386e000a2c636e7f
Kesara Rathnayake <kesara@kesara.lk>
2015-07-06 23:57:52 +1200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# deepdreamer
-Python program that make use of Google's DeepDream (https://github.com/google/deepdream/)
+# Deep Dreamer
+Python program that make use of [Google's DeepDream](https://github.com/google/deepdream/)
+
+## Requirements
+* Python 2.7
+* [NumPy](https://pypi.python.org/pypi/numpy)
+* [SciPy](https://pypi.python.org/pypi/scipy/)
+* [Pillow](https://pypi.python.org/pypi/Pillow/0)
+* [Caffe](http://caffe.berkeleyvision.org/)
+
+## Installation
+1. Install [NumPy](https://pypi.python.org/pypi/numpy), [SciPy](https://pypi.python.org/pypi/scipy/), [Pillow](https://pypi.python.org/pypi/Pillow/0) and [Caffe](http://caffe.berkeleyvision.org/). *NumPy, SciPy & Pillow can be installed via PIP.*
+2. Download **deploy.prototxt** from [bvlc_googlenet](https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet).
+3. Add line `force_backward: true` to **deploy.prototxt** file.
+4. Download **bvlc_googlenet.caffemodel** from [bvlc_googlenet](https://github.com/BVLC/caffe/tree/master/models/bvlc_googlenet).
+5. Make sure those two files are on root directory of DeepDreamer.
+
+## Usage
---
+++ deepdreamer/deepdreamer.py
@@ -0,0 +1,117 @@
+###############################################################################
+# Deep Dreamer
+# Based on https://github.com/google/deepdream/blob/master/dream.ipynb
+# Author: Kesara Rathnayake ( kesara [at] kesara [dot] lk )
+###############################################################################
+
+import numpy as np
+from caffe import Classifier
+from scipy.ndimage import affine_transform, zoom
+from PIL.Image import fromarray as img_fromarray, open as img_open
+
+NET_FN = "deploy.prototxt"  # Make sure force_backward: true
+PARAM_FN = "bvlc_googlenet.caffemodel"
+CHANNEL_SWAP = (2, 1, 0)
+# ImageNet mean, training set dependent
+CAFFE_MEAN = np.float32([104.0, 116.0, 122.0])
+
+
+def _preprocess(net, img):
+    return np.float32(np.rollaxis(img, 2)[::-1]) - net.transformer.mean["data"]
+
+
+def _deprocess(net, img):
+    return np.dstack((img + net.transformer.mean["data"])[::-1])
+
+
+def _make_step(
+        net, step_size=1.5, end="inception_4c/output", jitter=32, clip=True):
+    """ Basic gradient ascent step. """
+
+    src = net.blobs["data"]
+    dst = net.blobs[end]
+
+    ox, oy = np.random.randint(-jitter, jitter+1, 2)
+
+    # apply jitter shift
+    src.data[0] = np.roll(np.roll(src.data[0], ox, -1), oy, -2)
+
+    net.forward(end=end)
+    dst.diff[:] = dst.data  # specify the optimization objective
+    net.backward(start=end)
+    g = src.diff[0]
+    # apply normalized ascent step to the input image
+    src.data[:] += step_size/np.abs(g).mean() * g
+    # unshift image
+    src.data[0] = np.roll(np.roll(src.data[0], -ox, -1), -oy, -2)
+
+    if clip:
+        bias = net.transformer.mean["data"]
+        src.data[:] = np.clip(src.data, -bias, 255-bias)
+
+
+def _deepdream(
+        net, base_img, iter_n=10, octave_n=4, octave_scale=1.4,
+        end="inception_4c/output", clip=True, **step_params):
+    # prepare base images for all octaves
+    octaves = [_preprocess(net, base_img)]
+
+    for i in xrange(octave_n-1):
+        octaves.append(zoom(
+            octaves[-1], (1, 1.0/octave_scale, 1.0/octave_scale), order=1))
+
+    src = net.blobs["data"]
+
+    # allocate image for network-produced details
+    detail = np.zeros_like(octaves[-1])
+
+    for octave, octave_base in enumerate(octaves[::-1]):
+        h, w = octave_base.shape[-2:]
+        if octave > 0:
+            # upscale details from the previous octave
+            h1, w1 = detail.shape[-2:]
+            detail = zoom(detail, (1, 1.0*h/h1, 1.0*w/w1), order=1)
+
+        src.reshape(1, 3, h, w)  # resize the network's input image size
+        src.data[0] = octave_base+detail
+
+        for i in xrange(iter_n):
+            _make_step(net, end=end, clip=clip, **step_params)
+
+            # visualization
+            vis = _deprocess(net, src.data[0])
+            if not clip:  # adjust image contrast if clipping is disabled
+                vis = vis*(255.0/np.percentile(vis, 99.98))
+
+        # extract details produced on the current octave
+        detail = src.data[0]-octave_base
+
+    # returning the resulting image
+    return _deprocess(net, src.data[0])
+
+
+def _show_progress(progress):
+    print("\r[{bar}] {progress}%".format(
+        bar="#"*(progress/10),
+        progress=progress))
+
+
+def deepdream(
+        img_path, scale_coefficient=0.05, irange=100, iter_n=10, octave_n=4,
+        octave_scale=1.4, end="inception_4c/output", clip=True):
+    img = np.float32(img_open(img_path))
+    s = scale_coefficient
+    h, w = img.shape[:2]
+
+    # Load DNN model
+    net = Classifier(NET_FN, PARAM_FN, CAFFE_MEAN, CHANNEL_SWAP)
+
+    for i in xrange(irange):
+        img = _deepdream(
+            net, img, iter_n=iter_n, octave_n=octave_n,
+            octave_scale=octave_scale, end=end, clip=clip)
+        PIL.Image.fromarray(np.uint8(frame)).save("{}_{}.jpg".format(
+            img_path, i))
+        _show_progress(i/irange*100)
+        img = affine_transform(
+            img, [1-s, 1-s, 1], [h*s/2, w*s/2, 0], order=1)
