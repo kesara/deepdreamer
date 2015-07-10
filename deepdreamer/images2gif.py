4b6d3fd149b27fc395575ca7552b613a99d5e813
Kesara Rathnayake <kesara.rathnayake@webdrive.co.nz>
2015-07-10 16:23:32 +1200

    images2gif bug fix to support Pillow.

--- deepdreamer/images2gif.py
+++ deepdreamer/images2gif.py
@@ -432,7 +432,7 @@ class GifWriter:
         # Obtain palette for all images and count each occurance
         palettes, occur = [], []
         for im in images:
-            palettes.append(getheader(im)[1])
+            palettes.append(im.palette.getdata()[1])
         for palette in palettes:
             occur.append(palettes.count(palette))
 
